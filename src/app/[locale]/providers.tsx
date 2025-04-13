3baaa62f82e0e4166c57a5810b43ee71cd2322b5
seprog <sebastianpoppenborg@gmail.com>
2025-04-13 13:14:17 +0200

    added suppress hydration warning

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -3,16 +3,11 @@
 import { HeroUIProvider } from '@heroui/react'
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
-import { useEffect, useState } from 'react'
 
 export function Providers({ children }: {
   children: React.ReactNode
 }) {
-  // TODO hydration error (https://github.com/heroui-inc/heroui/issues/2073#issuecomment-1953728672)
-  const [isClient, setIsClient] = useState(false)
-  useEffect(() => setIsClient(true), [])
-
-  return isClient ? (
+  return (
     <HeroUIProvider>
       <ThemeProvider attribute='class' enableSystem={true}>
         <SessionProvider>
@@ -20,5 +15,5 @@ export function Providers({ children }: {
         </SessionProvider>
       </ThemeProvider>
     </HeroUIProvider>
-  ) : <></>
+  )
 }
