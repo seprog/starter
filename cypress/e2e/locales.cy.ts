d2573a013bf72bafd698e1c97e7f81a328e8ba71
seprog <sebastianpoppenborg@gmail.com>
2025-06-01 08:03:25 +0200

    revoked cypress config changes

--- cypress/e2e/locales.cy.ts
+++ cypress/e2e/locales.cy.ts
@@ -1,8 +1,6 @@
-import { routing } from '@/i18n/routing'
-
 describe('Default locale redirect', () => {
   it('should redirect to appropriate locale page', () => {
     cy.visit('/')
-    cy.url().should('include', routing.defaultLocale)
+    cy.url().should('include', '/en')
   })
 })
