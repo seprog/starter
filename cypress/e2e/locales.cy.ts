9ecdd30bfc886ec28bb9397463499919d5fb3599
seprog <sebastianpoppenborg@gmail.com>
2025-06-01 07:54:09 +0200

    modified cypress config and tests

--- cypress/e2e/locales.cy.ts
+++ cypress/e2e/locales.cy.ts
@@ -1,6 +1,8 @@
+import { routing } from '@/i18n/routing'
+
 describe('Default locale redirect', () => {
   it('should redirect to appropriate locale page', () => {
     cy.visit('/')
-    cy.url().should('include', '/en')
+    cy.url().should('include', routing.defaultLocale)
   })
 })
