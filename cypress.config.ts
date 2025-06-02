9aa20a6c5ee25e8514c95ec649f25f427d08f38d
seprog <sebastianpoppenborg@gmail.com>
2025-06-02 23:41:05 +0200

    removed component testing due to being unstable

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,11 +4,5 @@ export default defineConfig({
   e2e: {
     setupNodeEvents() {},
     baseUrl: 'http://localhost:3000'
-  },
-  component: {
-    devServer: {
-      framework: 'next',
-      bundler: 'webpack'
-    }
   }
 })
