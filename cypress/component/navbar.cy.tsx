2ce43ec8e76eaa598d69e2f4bc53d2d65d73ae36
seprog <sebastianpoppenborg@gmail.com>
2025-06-02 22:20:18 +0200

    removed translation dependency

--- cypress/component/navbar.cy.tsx
+++ cypress/component/navbar.cy.tsx
@@ -1,10 +1,8 @@
 import { MainNavbar } from '@/app/[locale]/navbar'
-import { useTranslations } from 'next-intl'
 
 describe('Main Navbar', () => {
   it('displays title', () => {
-    const t = useTranslations('navbar')
     cy.mount(<MainNavbar />).screenshot()
-    cy.get('a').contains(t('title'))
+    cy.get('a').contains('Starter')
   })
 })
