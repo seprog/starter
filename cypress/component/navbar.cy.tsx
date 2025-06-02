0247cf8654453ca2abea156a90b3503b98c15ee0
seprog <sebastianpoppenborg@gmail.com>
2025-06-02 22:46:35 +0200

    changed <Navbar /> to <Home /> for debugging

--- cypress/component/navbar.cy.tsx
+++ cypress/component/navbar.cy.tsx
@@ -1,17 +1,14 @@
-import { MainNavbar } from '@/app/[locale]/navbar'
+import Home from '@/app/[locale]/page'
 
 describe('Main Navbar', () => {
   it('displays title', () => {
-    cy.mount(<MainNavbar />).screenshot()
+    cy.mount(<Home />).screenshot()
     cy.get('a').contains('Starter')
   })
 
   it('displays items', () => {
     cy.mount(
-      <MainNavbar items={[
-        { key: 'item1', content: <>Item 1</> },
-        { key: 'item2', content: <>Item 2</> }
-      ]} />
+      <Home />
     ).screenshot()
     cy.contains('Item 1')
     cy.contains('Item 2')
